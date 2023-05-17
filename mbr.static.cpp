409eb355c1838dbc39dd672e6bbfda4812e3c6bc
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-17 19:37:56 +0700

    destruktur dipanggil

--- mbr.static.cpp
+++ mbr.static.cpp
@@ -30,7 +30,7 @@ int main()
     mahasiswa mhs1("Lia Kurnia"); 
     mahasiswa mhs2("Asroni");
     mhs2.nim = 10;
-    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs3("Andi Kurniawan"); 
     mhs3.nim = 20;
     mahasiswa mhs4("Joko Purbo"); 
 
